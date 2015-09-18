b6f668779f662b5db2f7b71beae468eb711dbf82
waylau <waylau521@gmail.com>
2015-09-18 20:30:24 +0800

    add

--- src/main/java/com/waylau/h2/demos/server/EmbeddedModeTest.java
+++ src/main/java/com/waylau/h2/demos/server/EmbeddedModeTest.java
@@ -7,6 +7,9 @@ import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -39,6 +42,11 @@ public class EmbeddedModeTest {
 		ResultSetMetaData rsmd = null;
 		String sql = null;
 		int count = 0;
+		long timeStart = 0;
+		long timeEnd = 0;
+		long longTime = 0;
+		int countLong = 100000;
+		
 		Class.forName("org.h2.Driver");
 
 		Connection conn = DriverManager.getConnection("jdbc:h2:~/test", "sa",
@@ -54,13 +62,15 @@ public class EmbeddedModeTest {
 					+ ", NAME2 VARCHAR(255) , NAME3 VARCHAR(255) , NAME4 VARCHAR(255)"
 					+ ", NAME5 VARCHAR(255) , NAME6 VARCHAR(255) , NAME7 VARCHAR(255)"
 					+ ", NAME8 VARCHAR(255) , NAME9 VARCHAR(255) , NAME10 VARCHAR(255));");
+			
+	
+			
+			// INSERT PreparedStatement addBatch性能测试
+			timeStart = System.currentTimeMillis();
 			sql = "INSERT INTO TEST VALUES(?,?,?,?,?,?,?,?,?,?,?);";
 			preparedStatement = conn.prepareStatement(sql);
 			
-			// INSERT 性能测试
-			long timeStart = System.currentTimeMillis();
-			
-			for (int i=0; i<100000; i++){
+			for (int i=0; i< countLong; i++){
 				preparedStatement.setInt(1, i+1);
 				preparedStatement.setString(2, "20000"+1);
 				preparedStatement.setString(3, "20000"+1);
@@ -77,11 +87,12 @@ public class EmbeddedModeTest {
 			}
 
 			preparedStatement.executeBatch();  
+			preparedStatement.clearBatch();
 			
-			long timeEnd = System.currentTimeMillis();
-			long longTime = timeEnd - timeStart;
+			timeEnd = System.currentTimeMillis();
+			longTime = timeEnd - timeStart;
 			
-			logger.info( "INSERT TIME:" + longTime );
+			logger.info( "INSERT PreparedStatement addBatch TIME:" + longTime );
 
 			// QUERY 性能测试
 			timeStart = System.currentTimeMillis();
@@ -119,7 +130,77 @@ public class EmbeddedModeTest {
 			logger.info( "DELETE TIME:" + longTime );
 			logger.info( "count:" + count );
 			
- 
+			// INSERT Statement 性能测试
+			timeStart = System.currentTimeMillis();
+			
+			for (int i=0; i< countLong; i++){
+				int j = i+1;
+				sql = "INSERT INTO TEST VALUES(" + j + ",'waylau','waylau','waylau','waylau','waylau'"
+						+ ",'waylau','waylau','waylau','waylau','waylau');";
+				statement.executeUpdate(sql);
+			}
+			
+			timeEnd = System.currentTimeMillis();
+			longTime = timeEnd - timeStart;
+			
+			logger.info( "INSERT Statement TIME:" + longTime );
+			
+			// DELETE 性能测试
+			timeStart = System.currentTimeMillis();
+			
+			sql = "DELETE  TEST WHERE  NAME='waylau';";
+			count = statement.executeUpdate(sql);
+			
+			timeEnd = System.currentTimeMillis();
+			longTime = timeEnd - timeStart;
+			
+			logger.info( "DELETE TIME:" + longTime );
+			logger.info( "count:" + count );
+			
+			// INSERT PreparedStatement 性能测试
+			
+			timeStart = System.currentTimeMillis();
+			sql = "INSERT INTO TEST VALUES(?,?,?,?,?,?,?,?,?,?,?);";
+			preparedStatement = conn.prepareStatement(sql);
+			for (int i=0; i< countLong; i++){
+
+				
+				preparedStatement.setInt(1, i+1);
+				preparedStatement.setString(2, "20000"+1);
+				preparedStatement.setString(3, "20000"+1);
+				preparedStatement.setString(4, "20000"+1);
+				preparedStatement.setString(5, "20000"+1);
+				preparedStatement.setString(6, "20000"+1);
+				preparedStatement.setString(7, "20000"+1);
+				preparedStatement.setString(8, "20000"+1);
+				preparedStatement.setString(9, "20000"+1);
+				preparedStatement.setString(10, "20000"+1);
+				preparedStatement.setString(11, "20000"+1);
+				
+				count = preparedStatement.executeUpdate();
+				preparedStatement.clearParameters();
+			}
+			
+			timeEnd = System.currentTimeMillis();
+			longTime = timeEnd - timeStart;
+			
+			logger.info( "INSERT PreparedStatement TIME:" + longTime );
+			
+			// UPDATE 性能测试
+			timeStart = System.currentTimeMillis();
+			
+			sql = "UPDATE  TEST SET  NAME='waylau';";
+			count = statement.executeUpdate(sql);
+			
+			timeEnd = System.currentTimeMillis();
+			longTime = timeEnd - timeStart;
+			
+			logger.info( "UPDATE TIME:" + longTime );
+			logger.info( "count:" + count );
+			
+			
+			
+			
 		} finally {
 			if (preparedStatement != null) {
 				preparedStatement.close();
@@ -132,5 +213,6 @@ public class EmbeddedModeTest {
 			}
 		}
 	}
-
 }
+
+
